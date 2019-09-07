559d2204aa8a7073f99568fb447a1ceadf7affd6
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-09-07 10:12:34 -0700

    Adds ability to make ajax calls to sign up, log in, log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+
+    def current_user
+      return nil unless session[:session_token]
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+  
+    def require_logged_in!
+      if !logged_in?
+        render json: ['User not logged in'], status: 401
+      end
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+  
+    def login(user)
+      session[:session_token] = user.reset_session_token!
+      @current_user = user 
+      
+    end
+  
+    def logout
+      current_user.reset_session_token!
+      session[:session_token] = nil
+      current_user = nil
+    end
 end
