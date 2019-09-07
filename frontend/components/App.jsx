beacdae0ef20dd7b6a6ac184420bdc0c695aaf6f
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-09-07 11:10:12 -0700

    Renders app component

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>RainFlix App Page!</h1>
+    </div>
+);
+
+export default App;
--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as auth from './util/auth_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore()
 
     // testing
-    window.signup = auth.signup
-    window.login = auth.login
-    window.logout = auth.logout
     window.getState = store.getState
     window.dispatch = store.dispatch
     
-    ReactDOM.render(<h1>Welcome to RainFlix!</h1>, root)
+    ReactDOM.render(<Root store={ store } />, root)
 });
