5d24aadd83b11f43d3e9bd562d06ee383d3c965f
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-09-11 15:46:42 -0700

    Adds username uniqueness db constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_071319) do
+ActiveRecord::Schema.define(version: 2019_09_11_223610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_071319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_token"], name: "index_users_on_session_token"
-    t.index ["username"], name: "index_users_on_username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
